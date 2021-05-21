fd15bf1745cdc88f444c6cdfaac9b2034fcdd67d
jd1t25 <mayureshsaitwal25@gmail.com>
2021-05-21 22:52:26 +0530

    Notification Added

--- bin/audio-switcher.sh
+++ bin/audio-switcher.sh
@@ -7,9 +7,11 @@ CURRENT_SINK=$(pacmd stat | grep "Default sink name" | sed 's/.*: *//')
 	if [ "$CURRENT_SINK" == "alsa_output.usb-Conexant_CONEXANT_USB_AUDIO_000000000000-00.analog-stereo" ]
 	then
 		pactl set-default-sink 2
+		notify-send "Audio 2 Speakers"
 		exit 1
 	else
 		pactl set-default-sink 1
+		notify-send "Audio 2 Headphones"
 		exit 1
 	fi
 
--- bin/toggle_picom.sh
+++ bin/toggle_picom.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+
 if pgrep -x "picom" > /dev/null 2>&1 
 then 
 	killall -q picom
-#	xdo above -t "$(xdo id -N Bspwm -n root | sort | head -n 1)" $(xdo id -n tint2)
-	bspc wm -r
+  notify-send "Picom Deactivated"
 else
-#	xdo above -t "$(xdo id -N Bspwm -n root | sort | head -n 1)" $(xdo id -n tint2)
 	picom --experimental-backends &		
-	bspc wm -r
+  notify-send "Picom Activated" 
 fi
