8c77f4678c98e14660937ee92c1c989c01d91437
jd1t25 <mayureshsaitwal25@gmail.com>
2021-05-23 19:21:01 +0530

    Notification can be replaced

--- bin/audio-switcher.sh
+++ bin/audio-switcher.sh
@@ -7,12 +7,10 @@ CURRENT_SINK=$(pacmd stat | grep "Default sink name" | sed 's/.*: *//')
 	if [ "$CURRENT_SINK" == "alsa_output.usb-Conexant_CONEXANT_USB_AUDIO_000000000000-00.analog-stereo" ]
 	then
 		pactl set-default-sink 2
-		notify-send "Audio 2 Speakers"
-		exit 1
+		notify.sh "Audio 2" "Speakers" "" 2 update
 	else
 		pactl set-default-sink 1
-		notify-send "Audio 2 Headphones"
-		exit 1
+		notify.sh "Audio 2" "Headphones" "" 2 update
 	fi
 
 
