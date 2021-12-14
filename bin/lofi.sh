83f67ce1dea3ba585c6764cb23a527c4726c4b8a
jd1t25 <mayureshsaitwal25@gmail.com>
2021-12-15 01:48:38 +0530

    Mute Input Sink sync with Sink Name

--- bin/lofi.sh
+++ bin/lofi.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-chromium --user-data-dir=.config/lofi --class='m_on_browser' --incognito https://www.youtube.com/watch\?v\=5qap5aO4i9A &
+chromium --user-data-dir=.config/lofi --class='m_on_browser' --incognito https://www.youtube.com/watch\?v\=5qap5aO4i9A & > /dev/null 2>&1 
+
+#firefox --new-instance https://www.youtube.com/watch\?v\=5qap5aO4i9A -P "music" & > /dev/null 2>&1 
--- bin/mute_sink_inputs.sh
+++ bin/mute_sink_inputs.sh
@@ -1,26 +1,51 @@
 #! /bin/bash
 
 
+### find name of Sink Inputs
 function sink_input()
 {
-    echo "$(pulsemixer --list-sinks | awk '/Sink input/ {print $6}'| tr -d ,)"
+   # echo "$(pulsemixer --list-sinks | awk '/Sink input/ {print $6}'| tr -d ,)"
+   pacmd list-sink-inputs | grep "media.name" | grep -o '".*"' | sed 's/"//g' | cut -f1 -d"-"
 }
 
-# Rofi Browser select
-BROWSER=$(sink_input | rofi -dmenu)
+### Rofi Browser select
+BROWSER=$(sink_input | rofi -dmenu -format i)
 
 
-sink_input_id=$(pulsemixer --list-sinks | grep $BROWSER | awk '{print $4}' | tr -d ,)
+### find sink input id
+IFS=$'\n'
+sink_input_id_array=( $(pacmd list-sink-inputs | grep "index" | grep -o '[0-9]\+') )
 
-mute=$(pulsemixer --list-sinks | grep $BROWSER | awk '{print $8}' | tr -d ,)
+sink_input_id=${sink_input_id_array[$BROWSER]}
 
 
-if (($mute==0)) 
+### find sink input mute status
+sink_input_mute_array=( $(pacmd list-sink-inputs | grep "muted" | awk '{print $2}') )
+
+sink_input_mute_status=${sink_input_mute_array[$BROWSER]}
+
+
+### Mute | Unmute
+if [[ $sink_input_mute_status == "no" ]]
 then
-    pulsemixer --mute --id $sink_input_id
+   pacmd set-sink-input-mute $sink_input_id yes 
 else
-    pulsemixer --unmute --id $sink_input_id
+   pacmd set-sink-input-mute $sink_input_id no
 fi
+
+
+
+#sink_input_id=$(pulsemixer --list-sinks | grep $BROWSER | awk '{print $4}' | tr -d ,)
+#
+#mute=$(pulsemixer --list-sinks | grep $BROWSER | awk '{print $8}' | tr -d ,)
+#
+#
+#if (($mute==0)) 
+#then
+#    pulsemixer --mute --id $sink_input_id
+#else
+#    pulsemixer --unmute --id $sink_input_id
+#fi
 
 #echo $mute
 #pulsemixer --list-sinks | awk '/Chromium/ {print $4}'| tr -d ,
