f1e067005eaf52f40746487b83082ff8f2c13821
jd1t25 <mayureshsaitwal25@gmail.com>
2024-01-09 11:43:43 +0530

    added hypr borders

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -1,49 +1,49 @@
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-   			["<C-h>"] = "which_key",
-				["JK"] = "close",
-      },
-      n = {
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-u>"] = false,
+				["<C-d>"] = false,
+				["<C-h>"] = "which_key",
 				["JK"] = "close",
 			},
-    },
-    pickers = {
-		-- find_command = "rg,--ignore,--hidden,--files prompt_prefix=🔍";
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
-	},
-	extensions = {
-		 -- fzy_native = {
-		 -- override_generic_sorter = false,
-		 -- override_file_sorter = true,
-        -- }
-		fzf = {
-			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = true, -- override the generic sorter
-			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
+			n = {
+				["JK"] = "close",
+			},
 		},
-		-- Your extension configuration goes here:
-		-- extension_name = {
-		--   extension_config_key = value,
-		-- }
-		-- please take a look at the readme of the extension you want to configure
+		pickers = {
+			-- find_command = "rg,--ignore,--hidden,--files prompt_prefix=🔍";
+			-- Default configuration for builtin pickers goes here:
+			-- picker_name = {
+			--   picker_config_key = value,
+			--   ...
+			-- }
+			-- Now the picker_config_key will be applied every time you call this
+			-- builtin picker
+		},
+		extensions = {
+			-- fzy_native = {
+			-- override_generic_sorter = false,
+			-- override_file_sorter = true,
+			-- }
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				-- the default case_mode is "smart_case"
+			},
+			-- Your extension configuration goes here:
+			-- extension_name = {
+			--   extension_config_key = value,
+			-- }
+			-- please take a look at the readme of the extension you want to configure
+		},
 	},
-  },
-}
+})
 
 -- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+pcall(require("telescope").load_extension, "fzf")
 -- pcall(require("telescope").load_extension("media_files"))
